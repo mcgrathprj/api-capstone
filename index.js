1cc5cfb7590ce669f03fb7c975607b200649de9a
Peter McGrath <mcgrathprj@gmail.com>
2018-04-07 11:44:05 -0400

    fixed listener

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const Wikipedia_LookUp_URL = "https://en.wikipedia.org/w/api.php?action=query";
 
 const Twitter_LookUp_URL = "";
 
+let results; 
+
+//I enter an address in order to retrieve a list of elected officials.
 function listenForSearchTerms() {
   $(".js-search-form").submit(event => {
     event.preventDefault();
@@ -17,37 +20,40 @@ function listenForSearchTerms() {
   });
 }
 
+//When I click enter, I receive back the list. 
 function getDataFromCivicInfo(searchTerm, callback) {
     const query = {
-    address: `${searchTerm}`,
-    key: "AIzaSyB7v_1_16Ghfi_9YeAL9JpV4FwYihrFyfY",
+      address: `${searchTerm}`,
+      key: "AIzaSyB7v_1_16Ghfi_9YeAL9JpV4FwYihrFyfY"
     };
     $.getJSON(CivicInfo_LookUp_URL, query, callback);
-    console.log("`getDataFromCivicInfo` ran; address is " + query.address);
 }
 
+//I retrieve headlines related to the elected official.
 function getDataFromNewsAPI(official_name, callback) {
   const news_query = {
-  apiKey: "475f5eaec1844e1ca8241585ad85d2c6",
-  q:`${official_name}`,};
+    apiKey: "475f5eaec1844e1ca8241585ad85d2c6",
+    q:`${official_name}`
+  };
   $.getJSON(News_LookUp_URL, news_query, callback);
 }
 
 
 function displayRepresentatives(data) {
-  const results = data.officials.map((official, index) => renderResult(official));
-  $(".js-search-results").html(results);
+  results = data; 
+  const html = data.officials.map((official, index) => renderResult(official));
+  $(".js-search-results").html(html);
 }
 
 function renderResult(result) {
   return `
-      <p class="js-official-list-item"><a href="#" onClick = "renderOfficialPage(${result.name})">${result.name}</a></p>
+      <p class="js-official-list-item"><a href="#" onClick = "renderOfficialPage('${result.name}')">${result.name}</a></p>
   `;
 }
 
-function renderOfficialPage(official) {
+function renderOfficialPage(person) {
     console.log("renderOfficialPage ran");
-    $(".js-official-page").html(`<p>${result.name} is your official.</p>`);
+    $(".js-official-page").html(`<p>${person} is your official.</p>`);
 }
 
 listenForSearchTerms();
