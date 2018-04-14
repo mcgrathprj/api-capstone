9352c4ce3d4f708454d3c5ffeccc50ea72ebbfe5
Peter McGrath <mcgrathprj@gmail.com>
2018-04-14 10:27:31 -0400

    added some detail to the official page

--- index.js
+++ index.js
@@ -29,37 +29,64 @@ function getDataFromCivicInfo(searchTerm, callback) {
   $.getJSON(CivicInfo_LookUp_URL, query, callback);
 }
 
-//I retrieve headlines related to the elected official.
-function getDataFromNewsAPI(official_name, callback) {
-  const news_query = {
-    apiKey: "475f5eaec1844e1ca8241585ad85d2c6",
-    q:`${official_name}`
-  };
-  $.getJSON(News_LookUp_URL, news_query, callback);
-}
-
-
 function displayRepresentatives(data) {
   results = data; 
-  const html = data.offices.map((office, index) => renderOffice(office));
+  const html = data.offices.map((office, index) => renderOffice(office,index));
   $(".js-search-results").html(html);
 }
 
-function renderOffice(office) {
+function renderOffice(office, officeIndex) {
   let html = "";
   for (let i = 0; i < office.officialIndices.length; i++) {
     html += `
       <p class="js-official-list-item"><a href="#" 
-      onClick = "renderOfficialPage('${results.official[office.officialIndices[i]].name}')">${results.official[office.officialIndices[i]].name}</a></p>
+      onClick = "renderOfficialPage(${officeIndex},${office.officialIndices[i]})">${results.officials[office.officialIndices[i]].name}</a></p>
     `
   }
   return html;
 
 }
 
-function renderOfficialPage(person) {
-    console.log("renderOfficialPage ran");
-    $(".js-official-page").html(`<p>${person} is your official.</p>`);
+function renderOfficialPage(office, index) {
+    let has_urls = results.officials[index].hasOwnProperty("urls")
+    console.log(Object.keys(results.officials[index]))
+    $(".js-search-results").css("display","none");
+    $(".js-search-form").css("display","none");
+    if (has_urls == true) {
+    $(".js-official-page").html(`<img src="${results.officials[index].photoUrl}" class="js-headshot">
+      <h2>${results.officials[index].name}: ${results.offices[office].name}</h2>
+      <h3>Party: ${results.officials[index].party}</h3>
+      <h3>Website: <a href="${results.officials[index].urls[0]}">${results.officials[index].urls[0]}</a></h3>
+      `);
+  }
+  else {
+   $(".js-official-page").html(`<img src="${results.officials[index].photoUrl}" class="js-headshot">
+      <h2>${results.officials[index].name}: ${results.offices[office].name}</h2>
+      <h3>Party: ${results.officials[index].party}</h3>
+      `) 
+  }
+  for (let i = 0; i < results.officials[index].channels.length; i++){
+    $(".js-official-page").append(`
+      <h3>${results.officials[index].channels[i].type}: ${results.officials[index].channels[i].id}</h3> 
+      `)
+  }
+getDataFromNewsAPI(results.officials[index].name, renderHeadlines(results));
 }
 
+//I retrieve headlines related to the elected official.
+function getDataFromNewsAPI(official_name, callback) {
+  const news_query = {
+    apiKey: "475f5eaec1844e1ca8241585ad85d2c6",
+    q:`${official_name}`
+  };
+  $.getJSON(News_LookUp_URL, news_query, callback)
+}
+
+
+function renderHeadlines(data) {
+    $(".js-news-results").html("<h3>Recent Headlines</h3>")
+    for (let i = 0; i < results.articles.length; i++) {
+      $(".js-news-results").append(`<p><a href="${results.articles[i].url}">${results.articles[i].title}</a></p>`)
+    }
+}
 listenForSearchTerms();
